86cf559068b5c73920d16cfb2279789b56028e73
liyun <liyuncecil@gmail.com>
2018-04-24 12:28:25 -0700

    Add project files.

---
+++ CommunityAssist2017MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityAssist2017MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CommunityAssist2017MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommunityAssist2017MVC.MvcApplication" Language="C#" %>
