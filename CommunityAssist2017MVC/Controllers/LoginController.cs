7b924e7e3ac618cc4546e4b1e2c4ba039742f699
liyun <liyuncecil@gmail.com>
2018-05-01 12:21:59 -0700

    Login

---
+++ CommunityAssist2017MVC/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017MVC.Models;
+namespace CommunityAssist2017MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Email, PlainPassword")]LoginClass lc)
+
+        {
+
+            int results = db.usp_Login(lc.Email, lc.PlainPassword);
+            int revKey = 0;
+            Message msg = new Message();
+
+            if (results != -1)
+            {
+                var pkey = (from r in db.People
+
+                            where r.PersonEmail.Equals(lc.Email)
+
+                            select r.PersonKey).FirstOrDefault();
+
+                revKey = (int)pkey;
+
+                Session["PersonKey"] = revKey;
+
+                msg.MessageText = "Welcome, " + lc.Email;
+            }
+            else
+                msg.MessageText = "Invalid Login. Please try again or register.";
+            return View("Result", msg);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
---
+++ CommunityAssist2017MVC/Models/LoginClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2017MVC.Models
+{
+    public class LoginClass
+    {
+        public string Email { set; get; }
+
+        public string PlainPassword { set; get; }
+
+        // bundle them together 
+    }
+}
