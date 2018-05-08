3b800634f8fb3fb09decde246c317a3dd23ed021
liyun <liyuncecil@gmail.com>
2018-05-08 12:25:44 -0700

    Donation

---
+++ CommunityAssist2017MVC/Controllers/DonationController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017MVC.Models;
+
+namespace CommunityAssist2017MVC.Controllers
+{
+    public class DonationController : Controller
+    {
+        // GET: Donation
+        public ActionResult Index()
+        {
+            if (Session["PersonKey"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+                
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "PersonKey, DonationDate, DonationAmount, DonationConfirmationCode")] Donation d)
+        {
+            
+            
+            d.PersonKey = (int)Session["PersonKey"];
+            d.DonationDate = DateTime.Now;
+         
+            d.DonationConfirmationCode = Guid.NewGuid();
+
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            db.Donations.Add(d);
+            db.SaveChanges();
+
+            Message m = new Message("Your donation is successfully accepted.");
+            return View("Result", m);
+
+        }
+
+        public ActionResult Result(Message msg)
+
+        {
+
+            return View(msg);
+
+        }
+
+    }
+}
